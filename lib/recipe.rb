0dc8bc184a568e802b355203805c0bdef3b9074b
Steven Galvin and Sara Schultz <steven@>
2017-05-11 14:32:37 -0700

    remote whitespace

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:ingredients)
   has_and_belongs_to_many(:categories)
-  validates(:name, :presence => true, on: :update)
+  validates(:name, :presence => true)
 end
